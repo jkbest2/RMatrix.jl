94e75b594cd2e0ae0c75ba734779be7c1d4acf4c
John Best <jkbest@gmail.com>
2020-07-22 21:15:55 -0700

    Define rcopy for dtCMatrix → SparseMatrixCSC

--- src/RMatrix.jl
+++ src/RMatrix.jl
@@ -1,5 +1,18 @@
 module RMatrix
 
-# Write your package code here.
+using SparseArrays
+using RCall
+import RCall: rcopy, rcopytype
+
+function rcopy(::Type{SparseMatrixCSC}, m::Ptr{S4Sxp})
+    SparseMatrixCSC(rcopy(m[:Dim][1]),
+                    rcopy(m[:Dim][2]),
+                    rcopy(m[:p]) .+ 1,
+                    rcopy(m[:i]) .+ 1,
+                    rcopy(m[:x]))
+end
+function rcopytype(::Type{RCall.RClass{:dtCMatrix}}, m::Ptr{S4Sxp})
+    SparseMatrixCSC
+end
 
 end
